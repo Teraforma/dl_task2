597fa917bee03588dd63915e87530fea62e48719
d.voloshyn0@hm.edu <user.email danil.voloshyn.work@gmail.com>
2026-06-18 22:49:22 +0200

    fit: add deployment README to calculate on linux remote
    add saves for the training
    fixes bugs
    do it all in jupyter notebook as well

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 1. setup enviroment
 ```bash
+uv python install 3.12
+uv python pin 3.12
 uv sync
-
+uv venv
+source 
 source .venv/bin/activate   # Linux/macOS
+#or in fish console 
+source .venv/bin/activate.fish
+
 .venv\Scripts\activate      # Windows
 
 python main.py
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def main():
+def train():
 
     import torchvision
     import numpy, sklearn
@@ -8,15 +8,24 @@ def main():
     from data.pets_dataset import get_dataloaders
     from models.classifier import PetClassifier
 
-
+    from pytorch_lightning.callbacks import ModelCheckpoint
     # add test loader
     train_loader, val_loader = get_dataloaders()
 
     model = Alpha(num_classes=37)
     lightning_model = PetClassifier(model)
 
+    checkpoint_callback = ModelCheckpoint(
+        dirpath="checkpoints/",
+        filename="pet-model--{epoch:02d}-{val_loss:.2f}",
+        save_top_k=3,
+        monitor="val_loss",
+        mode="min",
+    )
+
     trainer = L.Trainer(
         max_epochs=20,
+        callbacks=[checkpoint_callback],
     )
 
     trainer.fit(
@@ -25,5 +34,21 @@ def main():
         val_dataloaders=val_loader,
     )
 
+    # print("Best model:", checkpoint_callback.best_model_path)
+def searchBest():
+    # print("Best model:", checkpoint_callback.best_model_path)
+    # %%
+    import optuna
+    from evaluation.optuna_serch4hyper import objective
+
+    study = optuna.create_study(direction="maximize")
+
+    study.optimize(objective, n_trials=20)
+
+    print(study.best_params)
+
+
 if __name__ == "__main__":
-    main()
+    train()
+
+    searchBest()
